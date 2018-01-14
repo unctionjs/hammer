e1427dc48c69a505926522f6902549322582a948
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-01-14 00:12:35 -0800

    Removing a deprecated dependency

--- index.js
+++ index.js
@@ -4,19 +4,16 @@ import type from "@unction/type"
 import isType from "@unction/istype"
 import mergeRight from "@unction/mergeright"
 
-export default function hammer (key: KeyType): Function {
-  const keychain = keyChain([key])
-  const omitKey = exceptKey(key)
+import type {UnaryFunctionType} from "types"
+import type {RecordTreeType} from "types"
+import type {KeyType} from "types"
 
+export default function hammer (key: KeyType): UnaryFunctionType {
   return function hammerKey (recordTree: RecordTreeType): RecordTreeType {
     if (!(isType("Object")(recordTree) || isType("Map")(recordTree))) {
       throw new Error(`hammer only works on an Record types, like Object or Map, but the subset was a ${type(recordTree)}`)
     }
 
-    const only = keychain(recordTree)
-
-    const without = omitKey(recordTree)
-
-    return mergeRight(without)(only)
+    return mergeRight(exceptKey(key)(recordTree))(keyChain([key])(recordTree))
   }
 }
