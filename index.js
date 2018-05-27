c22be6d1ca95bd92eacd95e7a287938204a3d9e8
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-27 14:36:37 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-import dig from "@unction/dig"
-import exceptKey from "@unction/exceptkey"
-import mergeRight from "@unction/mergeright"
-
-import type {UnaryFunctionType} from "types"
-import type {KeyedEnumerableType} from "types"
-import type {KeyType} from "types"
-
-export default function hammer (key: KeyType): UnaryFunctionType {
-  return function hammerKey (keyedEnumerable: KeyedEnumerableType): KeyedEnumerableType {
-    return mergeRight(exceptKey(key)(keyedEnumerable))(dig([key])(keyedEnumerable))
-  }
+import dig from "@unction/dig";
+import exceptKey from "@unction/exceptkey";
+import mergeRight from "@unction/mergeright";
+export default function hammer(key) {
+  return function hammerKey(keyedEnumerable) {
+    return mergeRight(exceptKey(key)(keyedEnumerable))(dig([key])(keyedEnumerable));
+  };
 }
